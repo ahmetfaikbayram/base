12ae535e3689fa077540fcff236b2016fb12ec95
ahmetfaikbayram <ahmetfaikbayram@gmail.com>
2021-03-30 20:39:46 +0300

    Specification pattern

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,7 +23,9 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProductList()
         {
-            var products = await _unitOfWork.Repository<Product>().GetList();
+            var spec = new ProductsWithBrandSpecification();
+
+            var products = await _unitOfWork.Repository<Product>().GetListWithSpec(spec);
 
             return Ok(products);
         }
@@ -30,7 +33,9 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _unitOfWork.Repository<Product>().GetById(id);
+            var spec = new ProductsWithBrandSpecification(id);
+
+            return await _unitOfWork.Repository<Product>().GetByIdWithSpec(spec);
         }
     }
 }
